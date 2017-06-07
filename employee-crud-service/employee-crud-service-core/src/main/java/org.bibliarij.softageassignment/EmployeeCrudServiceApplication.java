061aa55b6fe0b387986463a336bbfe98452aef4f
unknown <astartes91@gmail.com>
2017-06-08 00:28:10 +0600

    Main application class added

---
+++ employee-crud-service/employee-crud-service-core/src/main/java/org.bibliarij.softageassignment/EmployeeCrudServiceApplication.java
@@ -0,0 +1,15 @@
+package org.bibliarij.softageassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by unknown on 08.06.2017.
+ */
+@SpringBootApplication
+public class EmployeeCrudServiceApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EmployeeCrudServiceApplication.class, args);
+    }
+}

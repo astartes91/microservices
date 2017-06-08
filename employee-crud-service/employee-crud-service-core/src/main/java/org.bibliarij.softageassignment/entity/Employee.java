4d7470ca308565453966ef2fec696af4abe4d061
Vladimir Nizamutdinov <vnizamutdinov@sanscrit.kz>
2017-06-08 17:51:03 +0600

    Add Employee JPA entity

---
+++ employee-crud-service/employee-crud-service-core/src/main/java/org.bibliarij.softageassignment/entity/Employee.java
@@ -0,0 +1,10 @@
+package org.bibliarij.softageassignment.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Vladimir Nizamutdinov on 08/June/2017.
+ */
+@Entity
+public class Employee {
+}
